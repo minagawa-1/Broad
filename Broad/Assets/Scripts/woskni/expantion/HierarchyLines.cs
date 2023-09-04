5cf7468064e030b3b45eb2933c0c926588207374
Woskni <sHourai@softbank.ne.jp>
2023-09-04 09:06:06 +0900

    デッキシーンの不具合修正
    [Add] URLを開くミートボールメニューをGUI表示する属性を作成
    [Add] ヒエラルキーウィンドウの各オブジェクトの親子・兄弟関係を罫線素片で見やすくする拡張メソッドを作成
    [Fix] デッキシーンのUIが正常に表示されない不具合を修正

---
+++ Broad/Assets/Scripts/woskni/expantion/HierarchyLines.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+using UnityEditor;
+
+[InitializeOnLoad]
+public class HierarchySymbols : MonoBehaviour
+{
+    public static bool drawing = true;
+
+    static HierarchySymbols()
+    {
+        if (drawing) EditorApplication.hierarchyWindowItemOnGUI += DrawHierarchySymbols;
+    }
+
+    private static void DrawHierarchySymbols(int instanceID, Rect selectionRect)
+    {
+        GameObject gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+
+        if (gameObject != null && gameObject.transform.parent != null)
+                SymbolsField(selectionRect, GetSymbols(gameObject.transform));
+    }
+
+    /// <summary>Transformの親子関係から罫線素片を取得</summary>
+    /// <param name="transform"></param>
+    /// <returns>表示用の罫線素片の配列</returns>
+    private static string[] GetSymbols(Transform transform)
+    {
+        int depth = GetHierarchyDepth(transform);
+
+        List<string> symbols = new List<string>();
+
+        // ("│")
+        for (int i = 0; i < depth - 1; ++i) symbols.Add(!IsLast(GetAncestor(transform, depth - i - 1)) ? "│" : "　");
+
+
+        // ("└", "├")
+        if (transform.childCount == 0) symbols.Add(IsLast(transform) ? "└" : "├");
+
+        return symbols.ToArray();
+    }
+
+    /// <summary>ヒエラルキー上の深さ（先祖の長さ）を取得</summary>
+    private static int GetHierarchyDepth(Transform transform)
+    {
+        int depth = 0;
+        while (transform.parent != null)
+        {
+            depth++;
+            transform = transform.parent;
+        }
+        return depth;
+    }
+
+    /// <summary>複数の罫線素片をひとつの文字列に変換する</summary>
+    private static void SymbolsField(Rect selectionRect, params string[] symbols)
+    {
+        const int indent = 59;
+        const int interval = 14;
+
+        for(int i = 0; i < symbols.Length; ++i)
+        {
+            float x = i * interval + indent;
+
+            Rect labelRect = new Rect(x, selectionRect.y, interval, selectionRect.height);
+
+            GUIStyle style = new GUIStyle();
+            style.normal.textColor = EditorTheme.GetIconColor().GetAlphaColor(0.5f);
+            style.fontSize = 15;
+
+            EditorGUI.LabelField(labelRect, symbols[i], style);
+        }
+    }
+
+    /// <summary>そのTransformの先祖を調べる</summary>
+    /// <param name="degree">何親等さかのぼるか（1: 親, 2: 祖親, 3: 曾祖親 …)</param>
+    /// <returns></returns>
+    private static Transform GetAncestor(Transform transform, int degree)
+    {
+        for (int i = 0; i < degree; ++i) transform = transform.parent;
+
+        return transform;
+    }
+
+    /// <summary>そのTransformが兄弟関係において末弟かどうか</summary>
+    private static bool IsLast(Transform transform) => transform.GetSiblingIndex() == transform.parent.childCount - 1;
+}
---
+++ Broad/Assets/Scripts/woskni/expantion/attribute/URL.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEditor.Localization.Editor;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+public class URLAttribute : PropertyAttribute
+{
+    public string url { get; private set; }
+
+    /// <summary>フィールド上にコメントを付加</summary>
+    /// <param name="comment">コメントテキスト</param>
+    /// <param name="space">上のフィールドとコメントの余白幅</param>
+    public URLAttribute(string url)
+    {
+        this.url = url;
+    }
+}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(URLAttribute))]
+public class URLDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        URLAttribute urlAttribute = (URLAttribute)attribute;
+
+        float line = EditorGUIUtility.singleLineHeight * 1.2f;
+
+        // プロパティフィールドの幅
+        Rect fieldRect = new Rect(position.x, position.y, position.width - line, position.height);
+        Rect buttonRect = new Rect(position.x + position.width - line, position.y, line, position.height);
+
+        // プロパティフィールドを表示
+        EditorGUI.PropertyField(fieldRect, property, label);
+
+        GUIStyle helpButtonStyle = EditorStyles.miniButton;
+
+        if (GUI.Button(buttonRect, new GUIContent("？", $"以下のURLを開きます:\n{urlAttribute.url}"), helpButtonStyle))
+            Application.OpenURL(urlAttribute.url);
+
+        EditorGUI.EndProperty();
+    }
+}
+#endif
